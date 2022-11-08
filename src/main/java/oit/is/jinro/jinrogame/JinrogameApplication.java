2bf5044005ad9777233d71c5d1e5f6ba9093d9d2
middlevalley <e1b20074@oit.ac.jp>
2022-11-08 15:52:11 +0900

    初期化

---
+++ src/main/java/oit/is/jinro/jinrogame/JinrogameApplication.java
@@ -0,0 +1,13 @@
+package oit.is.jinro.jinrogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinrogameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinrogameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/jinro/jinrogame/JinrogameApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.jinro.jinrogame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinrogameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
